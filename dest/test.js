214ebe81a1bb489e708d78444ca13a56bf5c48fa
jackieLin <dashi_lin@163.com>
2019-11-21 23:34:31 +0800

    fix：解决箭头函数的问题

--- dest/test.js
+++ dest/test.js
@@ -1,14 +1,25 @@
-// root.pendingInteractionMap.forEach((scheduledInteractions, scheduledExpirationTime) => {
-//    console.log(22) 
-// });
-root.pendingInteractionMap.forEach(function aa(scheduledInteractions, scheduledExpirationTime) {// if (scheduledExpirationTime <= expirationTime) {
-  //     scheduledInteractions.forEach(interaction =>
-  //         interactions.add(interaction),
-  //     );
-  // }
-
+root.pendingInteractionMap.forEach((scheduledInteractions, scheduledExpirationTime) => {
+  try {
+    console.group("null")
+    console.log(22);
+  } finally {
+    console.groupEnd("null");
+  }
+});
+root.pendingInteractionMap.forEach(function aa(scheduledInteractions, scheduledExpirationTime) {
   try {
     console.group("aa")
+
+    if (scheduledExpirationTime <= expirationTime) {
+      scheduledInteractions.forEach(interaction => {
+        try {
+          console.group("null")
+          return interactions.add(interaction);
+        } finally {
+          console.groupEnd("null");
+        }
+      });
+    }
   } finally {
     console.groupEnd("aa");
   }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-// root.pendingInteractionMap.forEach((scheduledInteractions, scheduledExpirationTime) => {
-//    console.log(22) 
-// });
+root.pendingInteractionMap.forEach((scheduledInteractions, scheduledExpirationTime) => {
+   console.log(22) 
+});
 
 root.pendingInteractionMap.forEach(function aa(scheduledInteractions, scheduledExpirationTime) {
     if (scheduledExpirationTime <= expirationTime) {
