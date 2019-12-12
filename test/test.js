daf2ab0def3ef1943c19c6628b22c1d209ea5bfb
jackieLin <dashi_lin@163.com>
2019-12-12 23:55:59 +0800

    提交

--- test/test.js
+++ test/test.js
@@ -1,11 +1,30 @@
-root.pendingInteractionMap.forEach((scheduledInteractions, scheduledExpirationTime) => {
-   console.log(22) 
-});
+// 先写测试用例，然后写 demo
+// 普通函数
+var aa = function() { }
+function aa() { }
 
-root.pendingInteractionMap.forEach(function aa(scheduledInteractions, scheduledExpirationTime) {
-    if (scheduledExpirationTime <= expirationTime) {
-        scheduledInteractions.forEach(interaction =>
-            interactions.add(interaction),
-        );
+() => { console.log(222)}
+
+var aa = {
+    bb: function() {},
+    cc: function name() { console.log(222) }
+};
+
+aa.prototype.bb = function () { console.log(222)};
+aa.prototype.bb = function name () { console.log(222)};
+
+[].forEach(function() {})
+
+try {
+    function name() {
+        console.log(222)
     }
-});
+} catch(e) {
+
+}
+
+function aa() {
+    return function () {
+        console.log(222)
+    }
+}
